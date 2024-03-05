6288e7900d12be269339fc26fb38740762f99f12
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-04 21:41:53 -0300

    Aula 02

--- script.js
+++ script.js
@@ -6,14 +6,44 @@ const curto_button = document.querySelector(".app__card-button--curto");
 
 const longo_button = document.querySelector(".app__card-button--longo");
 
+const banner_image = document.querySelector(".app__image");
+
+const banner_titulo = document.querySelector(".app__title");
+
 foco_button.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "foco");
+  alterarContexto("foco");
 });
 
 curto_button.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-curto");
+  alterarContexto("descanso-curto");
 });
 
-longo_button.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-})
+longo_button.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+});
+
+function alterarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  banner_image.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      banner_titulo.innerHTML = `
+      Otimize sua produtividade,<br />
+      <strong class="app__title-strong">mergulhe no que importa.</strong>
+        `;
+      break;
+    case "descanso-curto":
+      banner_titulo.innerHTML = `
+        Que tal dar uma respirada? <br /> 
+        <strong class="app__title-strong">Faça uma pausa curta!</strong>
+        `;
+      break;
+    case "descanso-longo":
+      banner_titulo.innerHTML = `
+        QHora de voltar à superfície. <br /> 
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>
+        `;
+    default:
+      break;
+  }
+}
