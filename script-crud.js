ffed810d953a57cc18ceb85b3d44ac434159ddff
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-13 20:38:29 -0300

    Update script-crud.js
    Feito Aula: 1.3 e 1.4

--- script-crud.js
+++ script-crud.js
@@ -1,9 +1,19 @@
 //Encontrar o botão 'Adicionar Tarefa'
-const btnAdicionarNovaTarefa = document.querySelector('.app__button--add-task');
-const formAdicionarTarefa = document.querySelector('.app__form-add-task');
-const textArea = document.querySelector('app__form-textarea');
+const btnAdicionarNovaTarefa = document.querySelector(".app__button--add-task");
+const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textArea = document.querySelector(".app__form-textarea");
 
+const listaDeTarefas = []
 
-btnAdicionarNovaTarefa.addEventListener('click', () => {
-    formAdicionarTarefa.classList.toggle('hidden')
-})
+btnAdicionarNovaTarefa.addEventListener("click", () => {
+  formAdicionarTarefa.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const tarefa = {
+    descricao: textArea.value,
+  };
+  listaDeTarefas.push(tarefa);
+  localStorage.setItem('tarefas', listaDeTarefas);
+});
